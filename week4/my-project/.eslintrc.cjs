53b6f933d2af0642e5207fd1f16a39345375a74b
hanseo <yds06206@gmail.com>
2024-05-09 19:12:30 +0900

    init : 초기세팅 리액트로 변경

--- week4/my-project/.eslintrc.cjs
+++ week4/my-project/.eslintrc.cjs
@@ -3,23 +3,20 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'some-other-config-you-use',
-    'prettier',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'prettier',
+    'some-other-config-you-use',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'prettier', 'airbnb'],
   rules: {
+    'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    '@typescript-eslint/strict-boolean-expressions': [
-      2,
-      {
-        allowString: false,
-        allowNumber: false,
-      },
-    ],
     'prettier/prettier': 'error',
   },
 };
